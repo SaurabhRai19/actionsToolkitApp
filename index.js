9a8894f38babcc2bb50ceb4e4ea89af67ef971ab
Saurabh Rai <saurabhrai473@gmail.com>
2022-03-24 15:33:19 +0530

    Added artifact package

--- index.js
+++ index.js
@@ -1,11 +1,15 @@
 const core = require('@actions/core');
 const github = require('@actions/github');
+const artifact = require('@actions/artifact');
+const artifactClient = artifact.create()
+
 
 async function run(){
 try {
   // `who-to-greet` input defined in action metadata file
   const nameToGreet = core.getInput('who-to-greet');
-  
+ 
+  //Octokit Package
   const myToken = core.getInput('myToken');
   const octokit = github.getOctokit(myToken)
   
@@ -15,6 +19,21 @@ try {
     title: 'Issue created from custom action app',
   });
   console.log(issue);
+  //--------------
+  //Atifact Package
+
+  const artifactName = 'my-artifact';
+const files = [
+    'C:\Users\Asus\Desktop\Github Actions\ActionsToolkitPackages\actionsToolkitApp\file1.txt'
+]
+const rootDirectory = 'C:\Users\Asus\Desktop\Github Actions\ActionsToolkitPackages\actionsToolkitApp'
+const options = {
+    continueOnError: true
+}
+
+const uploadResult = await artifactClient.uploadArtifact(artifactName, files, rootDirectory, options)
+
+  //
 
   console.log(`Hello ${nameToGreet}!`);
   const time = (new Date()).toTimeString();
